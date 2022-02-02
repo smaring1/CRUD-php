e40eeb7bab21334a79c6b6d45e034355de4c329f
Simón Marín Giraldo <53051453+smaring1@users.noreply.github.com>
2022-02-01 22:03:03 -0500

    Uploaded project

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>CRUD Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .container {
+            text-align: center;
+            border: 7px;
+            width: 300px;
+            height: 200px;
+            padding-top: 100px;
+        }
+
+        #btn {
+            font-size: 25px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <button id="btn" class="btn btn-primary my-5"><a href="display.php" class="text-light">Enter application</a></button>
+    </div>
+</body>
+
+</html>
